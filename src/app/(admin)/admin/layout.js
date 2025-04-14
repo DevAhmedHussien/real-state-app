6804602c44156b7db0b1ca709e1f117c4dd0dee4
Ahmed Hussien <130807970+DevAhmedHussien@users.noreply.github.com>
2025-04-15 00:02:05 +0500

    adin g

--- src/app/(admin)/admin/layout.js
+++ src/app/(admin)/admin/layout.js
@@ -1,14 +1,10 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import AppSidebar from "./page"
- 
+
 export default function AdminLayout({children}) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
+      <> 
       <main>
-        <SidebarTrigger />
         {children}
       </main>
-    </SidebarProvider>
+      </>
   )
 }
